2265c74955c523d1abd31806f9570ea7b18766a5
Ramakrishnamurthy Yeleswarapu <yrk0265@gmail.com>
2018-06-06 00:10:07 +0530

    Update Reverseofstring.cs

--- Reverseofstring.cs
+++ Reverseofstring.cs
@@ -1,1 +1,9 @@
+string str="this is it.";
+string Result=string.Empty;
+for(int intI=str.Length;intI>=0;intI--)
+{
+   Result+=Convert.ToString(str[intI]);
+}
+Console.WriteLine(Result);
+Console.ReadLine();
 
